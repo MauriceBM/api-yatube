d2db20b81a9109de33c88287f2eac03a9b23312d
Maurice <mauricelandryballamessi@gmail.com>
2026-06-07 07:16:54 +0300

    Corrections selon remarques reviewer

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,10 +2,7 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    """
-    Permission personnalisée qui permet la modification
-    uniquement à l'auteur de l'objet.
-    """
+    """Разрешение на изменение только для автора объекта."""
 
     def has_object_permission(self, request, view, obj):
         return (
